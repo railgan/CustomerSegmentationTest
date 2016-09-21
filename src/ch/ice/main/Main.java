8c288901f411a0db7c62bae1b58e79d2d8b5c5a4
railgan <sclol@hotmail.com>
2016-09-21 16:19:58 -0700

    First Commit
    Created ExcelParser that reads a XLSX file

---
+++ src/ch/ice/main/Main.java
@@ -0,0 +1,12 @@
+package ch.ice.main;
+
+import java.io.IOException;
+
+import ch.ice.file.ExcelParser;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		ExcelParser.readXLSXFile();
+	}
+}
