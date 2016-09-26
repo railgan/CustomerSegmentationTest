9caa346b921476ee331b3c050137458d90aa8745
railgan <sclol@hotmail.com>
2016-09-26 15:16:50 -0700

    Added ParserTests

--- src/ch/ice/compare/ListComparison.java
+++ src/ch/ice/compare/ListComparison.java
@@ -4,14 +4,15 @@ import java.util.ArrayList;
 
 public class ListComparison {
 
-	public String posCompany;
-	public String posSegment;
-	public void compareLists(ArrayList<String> Register, ArrayList<String> POS){
+	public static String posCompany;
+	public static String posSegment;
+	public static void compareLists(ArrayList<String> Register, ArrayList<String> POS){
 		for (int i = 0; i < POS.size(); i++) {
 			posCompany = (POS.get(i));
 			if(Register.contains(posCompany)){ 
 				posSegment = "Medical"; }
-			else{ posSegment = "other";}
+			else{ posSegment = "Other";}
+			System.out.println(posSegment);
 		}
  
 	}
--- src/ch/ice/main/Main.java
+++ src/ch/ice/main/Main.java
@@ -1,13 +1,16 @@
 	package ch.ice.main;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import ch.ice.compare.ListComparison;
 import ch.ice.file.ExcelParser;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-			ExcelParser.readRegisterFile();
-			ExcelParser.readPOSFile();
+			ArrayList<String> ListReg = ExcelParser.readRegisterFile();
+			ArrayList<String> ListPos = ExcelParser.readPOSFile();
+			ListComparison.compareLists(ListReg, ListPos);
 	}
 }
